8fb94ff0f0c23c1c7a32a9461d090aaa3f3aa0cb
Benjamin Macher <benjamin.dev@icloud.com>
2020-05-05 11:36:00 +0200

    refactor(client): move composition-api to @/plugins

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+
+import '@/plugins';
 import vuetify from '@/plugins/vuetify';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-Vue.use(VueCompositionApi);
 
 Vue.config.productionTip = false;
 
