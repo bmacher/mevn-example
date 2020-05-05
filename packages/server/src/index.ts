6107dab14a5fbc04d4375d485a30e048ac57fe58
Benjamin Macher <benjamin.dev@icloud.com>
2020-05-05 13:55:03 +0200

    feat(server): add GET, PUT & DELETE for /todos

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,13 +1,52 @@
 import express from 'express';
 import cors from 'cors';
-import { sayHello } from '@mevn/common';
+import bodyParser from 'body-parser';
+import { Todo, isTodo } from '@mevn/common';
 
 const app = express();
 
-app.use(cors());
+app
+  .use(cors())
+  .use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send(sayHello('World'));
+// Local "storage"
+let currentId = 1;
+let todos: Todo[] = [];
+
+app.get('/todos', (req, res) => {
+  if (todos.length > 0) {
+    res.send(todos);
+  } else {
+    res.sendStatus(204);
+  }
+});
+
+app.put('/todos', (req, res) => {
+  const { body } = req;
+
+  console.info(body);
+
+  if (isTodo(body)) {
+    todos = [...todos, { id: currentId, ...body }];
+    currentId += 1;
+
+    res.sendStatus(200);
+  } else {
+    res
+      .status(400)
+      .send('Invalid body');
+  }
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const todo = todos.find(({ id }) => id === Number(req.params.id));
+
+  if (todo) {
+    todos = todos.filter((_todo) => _todo !== todo);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(204);
+  }
 });
 
 const server = app.listen(3000, () => {
