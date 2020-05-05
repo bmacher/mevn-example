e5dc1eb36a0a76f74f43c11365deac5e7e9fc00c
Benjamin Macher <benjamin.dev@icloud.com>
2020-05-05 14:08:03 +0200

    chore(common): make todo test names more specific

--- packages/common/src/todo.spec.ts
+++ packages/common/src/todo.spec.ts
@@ -1,7 +1,7 @@
 import { Todo, isTodo } from './todo';
 
-describe('todo', () => {
-  it('should return true', () => {
+describe('isTodo', () => {
+  it('should return true when todo is valid', () => {
     const todo = {
       id: 1,
       task: 'New todo',
@@ -11,7 +11,7 @@ describe('todo', () => {
     expect(isTodo(todo)).toBe(true);
   });
 
-  it('should return false', () => {
+  it('should return false when missing "done"', () => {
     const todo = {
       id: 1,
       task: 'New todo',
@@ -20,7 +20,7 @@ describe('todo', () => {
     expect(isTodo(<Todo>todo)).toBe(false);
   });
 
-  it('should return false', () => {
+  it('should return false when missing "task"', () => {
     const todo = {
       id: 1,
       done: false,
